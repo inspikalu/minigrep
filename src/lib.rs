9453775674605cba4ffc5943feec1c66b8e32c3d
Clement Kalu Okereke <inspikalu@gmail.com>
2025-07-21 16:13:28 +0100

    Changed the program to use iterators where necessary

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::env::{self};
 use std::error::Error;
 use std::fs;
 
@@ -9,13 +9,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+        ) -> Result<Config, &'static str> {
+        args.next();
+        // if args.len() < 3 {
+        //     return Err("not enough arguments");
+        // }
+        //
+        // let query = args[1].clone();
+        // let file_path = args[2].clone();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+
+
         //TODO: Search up what the .is_ok() does
         //so apparently .is_ok() returns true if the Result is ok
         // .get(3) is used to get the element at the index of 3 it returns an Option enum
@@ -31,7 +37,20 @@ impl Config {
         */
 
         //The .map_or() Returns the provided default result (if none), or applies a function to the contained value (if any).
-        let ignore_case = env::var("IGNORE_CASE").is_ok() || args.get(3).map_or(false, |arg| arg == "insensitive");
+        // let ignore_case = env::var("IGNORE_CASE").is_ok() || args.get(3).map_or(false, |arg| arg == "insensitive");
+
+        let query = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path")
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         Ok(Config {
             query,
             file_path,
@@ -76,27 +95,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
-    //TODO: Research what the lines do
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(&line);
-        }
-    }
-    results
+    // let mut results: Vec<&str> = Vec::new();
+    // //TODO: Research what the lines do
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(&line);
+    //     }
+    // }
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results: Vec<&str> = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
